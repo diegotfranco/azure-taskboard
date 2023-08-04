511f67aba2825544b220ac542fe64b8a7b07e807
andrey067 <audrey.ernesto.lima@gmail.com>
2023-08-04 14:55:34 -0400

    criando configuracao basica de autenticacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import {
+  AuthenticationResult,
+  PublicClientApplication,
+} from '@azure/msal-browser';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'azure-taskboard';
+  //TODO - Passar para uma service
+  userAuth?: AuthenticationResult;
+  private publicClientApplication: PublicClientApplication =
+    new PublicClientApplication({
+      auth: {
+        clientId: environment.auth.clientId,
+        redirectUri: environment.auth.redirectUri,
+        authority: environment.auth.authority,
+      },
+      cache: {
+        cacheLocation: 'sessionStorage',
+        storeAuthStateInCookie: true,
+      },
+    });
+
+  async ngOnInit() {
+    let t = this.publicClientApplication.getAllAccounts();
+    
+
+    !t.length ?
+    this.userAuth = await this.publicClientApplication.loginPopup({
+      scopes: environment.auth.scopes,
+      prompt: 'select_account',
+    }):console.log('t', t);
+    
+  }
 }
