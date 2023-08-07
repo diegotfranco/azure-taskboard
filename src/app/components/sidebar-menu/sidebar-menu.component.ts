f0d530ad7fccb584189bc0f154996e76abd2acd3
Amanda Macedo Jaria <ajaria@inovvati.com.br>
2023-08-07 15:43:17 -0300

    deslogar

--- src/app/components/sidebar-menu/sidebar-menu.component.ts
+++ src/app/components/sidebar-menu/sidebar-menu.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-sidebar-menu',
   templateUrl: './sidebar-menu.component.html',
-  styleUrls: ['./sidebar-menu.component.css']
+  styleUrls: ['./sidebar-menu.component.css'],
 })
 export class SidebarMenuComponent implements OnInit {
+  constructor(private usuarioService: UsuarioService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async deslogar(): Promise<any> {
+    console.log('deslogou');
+    await this.usuarioService.logout();
   }
-
 }
