4fa2af74e8bce31962edb3b4b6e938611d7e5492
Ivanildodev <ivanildofernandes.dev@gmail.com>
2023-08-07 12:11:59 -0400

    Remoção de comentários e importações não utilizadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
-import {
-  AuthenticationResult,
-  PublicClientApplication,
-} from '@azure/msal-browser';
-import { environment } from '../environments/environment';
 import { UsuarioService } from './services/usuario.service';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -14,12 +8,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'azure-taskboard';
-  //TODO - Passar para uma service
-  constructor(private usuarioService: UsuarioService){} // constructor(private usuarioService: UsuarioService, private http: HttpClient){}
+  constructor(private usuarioService: UsuarioService){}
 
   async ngOnInit() {
-    const user = await this.usuarioService.login();
-    console.log('user', user);
-    console.log("token", this.usuarioService.getBearerToken())
+    await this.usuarioService.login();
   }
 }
