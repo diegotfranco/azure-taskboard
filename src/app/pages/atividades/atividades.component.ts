cd322f2ed7aa46538924067c3e8b7e551f1e00e5
Andre Orostica <andre.orostica@gmail.com>
2023-08-07 13:27:42 -0400

    Resize table

--- src/app/pages/atividades/atividades.component.ts
+++ src/app/pages/atividades/atividades.component.ts
@@ -1,28 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { ColDef } from 'ag-grid-community';
+import { ColDef, GridSizeChangedEvent } from 'ag-grid-community';
 
 @Component({
   selector: 'app-atividades',
   templateUrl: './atividades.component.html',
-  styleUrls: ['./atividades.component.css']
+  styleUrls: ['./atividades.component.css'],
 })
 export class AtividadesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   columnDefs: ColDef[] = [
     { field: 'make' },
     { field: 'model' },
-    { field: 'price' }
-];
+    { field: 'price' },
+  ];
 
-rowData = [
-  { make: 'Toyota', model: 'Celica', price: 35000 },
-  { make: 'Ford', model: 'Mondeo', price: 32000 },
-  { make: 'Porsche', model: 'Boxster', price: 72000 }
-];
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxster', price: 72000 },
+  ];
 
+  onGridSizeChanged(params: GridSizeChangedEvent) {
+    // get the current grids width
+    var gridWidth = document.getElementById('grid-wrapper')!.offsetWidth;
+    // keep track of which columns to hide/show
+    var columnsToShow = [];
+    var columnsToHide = [];
+    // iterate over all columns (visible or not) and work out
+    // now many columns can fit (based on their minWidth)
+    var totalColsWidth = 0;
+    var allColumns = params.columnApi.getColumns();
+    if (allColumns && allColumns.length > 0) {
+      for (var i = 0; i < allColumns.length; i++) {
+        var column = allColumns[i];
+        totalColsWidth += column.getMinWidth() || 0;
+        if (totalColsWidth > gridWidth) {
+          columnsToHide.push(column.getColId());
+        } else {
+          columnsToShow.push(column.getColId());
+        }
+      }
+    }
+    // show/hide columns based on current grid width
+    params.columnApi.setColumnsVisible(columnsToShow, true);
+    params.columnApi.setColumnsVisible(columnsToHide, false);
+    // fill out any available space to ensure there are no gaps
+    params.api.sizeColumnsToFit();
+  }
 }
