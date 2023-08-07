f607055204bbfae5256c8a1b2bb240e11380ab02
Amanda Macedo Jaria <ajaria@inovvati.com.br>
2023-08-07 14:31:33 -0300

    ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
-    production: true,
-    auth: {
-        clientId: '',
-        redirectUri: '',
-        authority:'',
-        scopes: [],
-    },
+  production: false,
+  auth: {
+    clientId: '4aac7ebd-25d3-4066-b6ca-7c8defd75099',
+    redirectUri: 'http://localhost:4200/',
+    authority:
+      'https://login.microsoftonline.com/4b4804e7-608f-4dbc-9547-7ccc5d9b8600',
+    scopes: ['user.read', 'tasks.read'],
+  },
 };
-  
