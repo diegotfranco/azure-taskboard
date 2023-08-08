ec4c7186cfc0692659472de002ce0de85e4b9106
andrey067 <audrey.ernesto.lima@gmail.com>
2023-08-08 16:06:11 -0400

    alterando production value

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   auth: {
     endpoint: 'https://dev.azure.com/dr34mt34m/_apis/projects?api-version=6.0',
     clientId: '4aac7ebd-25d3-4066-b6ca-7c8defd75099',
