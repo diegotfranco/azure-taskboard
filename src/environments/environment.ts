0b4efa9218fa2e778ce48fb74c543320384be3e1
Andre Orostica <andre.orostica@gmail.com>
2023-08-08 08:27:37 -0400

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,9 @@
 export const environment = {
   production: false,
   auth: {
-    clientId: '4aac7ebd-25d3-4066-b6ca-7c8defd75099',
-    redirectUri: 'http://localhost:4200/',
-    authority:
-      'https://login.microsoftonline.com/4b4804e7-608f-4dbc-9547-7ccc5d9b8600',
-    scopes: ['user.read', 'tasks.read'],
+    clientId: '',
+    redirectUri: '',
+    authority: '',
+    scopes: [],
   },
 };
