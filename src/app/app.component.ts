17fadc01204d7826e3f3d54f8fb8d00a2a80db4b
andrey067 <audrey.ernesto.lima@gmail.com>
2023-08-08 09:42:23 -0400

    Testes de Enviroments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,26 @@ export class AppComponent {
   title = 'azure-taskboard';
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(
+      'auth',
+      typeof environment.auth.clientId,
+      environment.auth.clientId
+    );
+    console.log(
+      'auth',
+      typeof environment.auth.redirectUri,
+      environment.auth.redirectUri
+    );
+    console.log(
+      'auth',
+      typeof environment.auth.authority,
+      environment.auth.authority
+    );
+    console.log(
+      'auth',
+      typeof environment.auth.scopes,
+      environment.auth.scopes
+    );
+  }
 }
